8dd5459f6cfe491bae497069656ae7a702250bc7
NewbieErrorSystemIDN <56470811+NewbieErrorSystemIDN@users.noreply.github.com>
2019-10-13 09:55:35 +0700

    Add files via upload

---
+++ DarkNewbie-Tool.py
@@ -0,0 +1,157 @@
+Skip to content
+ Ranginang67 / DarkFly-Tool
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+Code Issues 4 Pull requests 2 Projects 0 Wiki Security Pulse Community
+DarkNewbie-Tool/install.py
+@CHARLEz YT NewbieErrorSystemIDN Update install.py
+cf9a0a9 on 12 Mar
+135 lines (102 sloc)  4.79 KB
+ 
+#!/usr/bin/python
+
+"""
+Author             : Charles David Saputra
+contact            : ambari.developer@gmail.com
+Github             : https://github.com/NewbieErrorSystemIDN
+my youtube channel : CHARLEz YT
+subcribe my youtube Channel to learn ethical Hacking ^_^
+"""
+
+import sys
+import os.path
+import subprocess
+
+ntfile = ['.module', 'lib']
+
+ubuntu = '' + str(open('.module/jalurU.ms').read())
+termux = '' + str(open('.module/jalurT.ms').read())
+termlb = '' + str(open('.module/ngentot.ms').read())
+instal = '' + str(open('.module/IN.ms').read())
+
+
+def _main_():
+    if os.path.isdir('' + ubuntu.strip()):
+        if os.getuid() != 0:
+            print '[x] Failed: your must be root'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[0]):
+            print '[x] Failed: no directory module'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[1]):
+            print '[x] Failed: no directory lib'
+            sys.exit()
+        else:
+
+            print '' + instal.strip()
+
+            # ==================================================================#
+
+            os.system('python .module/files/' + str(open('.module/A.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/B.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/C.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/D.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/E.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/F.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/G.ms'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/H.ms'
+                      ).read()))
+
+            # ==================================================================#
+
+            if os.path.isdir('/usr/bin/lib'):
+                os.system('rm -rf /usr/bin/lib')
+                os.system('' + str(open('.module/pindahU.ms').read()))
+                os.system('' + str(open('.module/PindahU.ms').read()))
+
+            if not os.path.isdir('/usr/bin/lib'):
+                os.system('' + str(open('.module/pindahU.ms').read()))
+                os.system('' + str(open('.module/PindahU.ms').read()))
+
+            # ==================================================================#
+
+            print '' + str(open('.module/DU.la').read())
+            print '' + str(open('.module/Du').read())
+            os.system('python .JM.xn')
+
+            # ==================================================================#
+
+    if os.path.isdir('' + termux.strip()):
+        if not os.path.isdir('' + termux.strip()):
+            sys.exit()
+
+        if not os.path.isdir(ntfile[0]):
+            print '[x] Failed: no directory module'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[1]):
+            print '[x] Failed: no directory lib'
+            sys.exit()
+        else:
+
+            print '' + instal.strip()
+
+            # ==============================================================#
+
+            os.system('python2 .module/' + str(open('.module/A.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/B.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/C.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/D.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/E.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/F.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/G.ms'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/H.ms'
+                      ).read()))
+
+            # ==============================================================#
+
+            if os.path.isdir('' + termlb.strip()):
+                os.system('' + str(open('.module/pacar.ms').read()))
+                os.system('' + str(open('.module/pindahT.ms').read()))
+                os.system('' + str(open('.module/PINDAHT.txt').read()))
+
+            if not os.path.isdir('' + termlb.strip()):
+                os.system('' + str(open('.module/pindahT.ms').read()))
+                os.system('' + str(open('.module/PINDAHT.txt').read()))
+
+            # ==============================================================#
+
+            print '' + str(open('.module/DU.la').read())
+            print '' + str(open('.module/Du').read())
+            os.system('python2 .JM.xn && cd')
+
+
+            # ==============================================================#
+
+if __name__ == '__main__':
+    _main_()
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
